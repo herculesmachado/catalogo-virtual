1b9f48f79c53c94076ac7e78d7f8bd297370e9f4
Hercules Machado <herculesprogramador911@gmail.com>
2024-11-17 11:36:55 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <header id='header'>
+        <div className='container-header'>
+          <h1 className='title'>CATÁLOGO <br /> VIRTUAL</h1>
+          <p className='subtitle'>MAQUIAGEM & <br /> VARIEDADES</p>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
